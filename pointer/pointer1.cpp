95aae185fec5b5ac9b84d5dadfba5cd8a8f181df
sumitvijaykumarr <sumitvijaykumarr@gmail.com>
2025-08-25 20:16:58 +0530

    pointer updated

--- pointer/pointer1.cpp
+++ pointer/pointer1.cpp
@@ -6,8 +6,17 @@ int main(){
     ptr = &a;
     std::cout<<*ptr<<std::endl;
     std::cout<<*ptr++<<std::endl;
+    std::cout<<a<<std::endl;
     std::cout<<*ptr<<std::endl;
 
     std::cout<<*(ptr++)<<std::endl;
+    std::cout<<(*ptr)++<<std::endl;
+
+    int arr[] = {3,4,5,6,7};
+    std::cout<<*arr<<std::endl;
+    std::cout<<*arr+1<<std::endl;
+    std::cout<<arr<<std::endl;
+    std::cout<<++*arr<<std::endl;
+    
     return 0;
 }
